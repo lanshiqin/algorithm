f299ee826dd8576b76240e129966e5ed7d8bafca
lanshiqin <lanshiqin@outlook.com>
2021-12-24 23:53:15 +0800

    160. 相交链表

--- app/src/main/java/com/lanshiqin/algorithm/leetcode/a160/Solution.java
+++ app/src/main/java/com/lanshiqin/algorithm/leetcode/a160/Solution.java
@@ -93,4 +93,17 @@ public class Solution {
         }
         return null;
     }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        while (l1 != l2) {
+            l1 = l1 == null ? headB : l1.next;
+            l2 = l2 == null ? headA : l2.next;
+        }
+        return l1;
+    }
 }
--- app/src/test/java/com/lanshiqin/algorithm/leetcode/a160/SolutionTest.java
+++ app/src/test/java/com/lanshiqin/algorithm/leetcode/a160/SolutionTest.java
@@ -44,4 +44,37 @@ class SolutionTest {
             result = result.next;
         }
     }
+
+    @Test
+    void getIntersectionNode2() {
+        int[] tailArray = {8, 4, 5};
+        ListNode tailPreNode = new ListNode();
+        ListNode tailPre = tailPreNode;
+        for (int value : tailArray) {
+            tailPreNode.next = new ListNode(value);
+            tailPreNode = tailPreNode.next;
+        }
+        int[] a = {4, 1};
+        ListNode listAPrev = new ListNode();
+        ListNode listA = listAPrev;
+        for (int value : a) {
+            listAPrev.next = new ListNode(value);
+            listAPrev = listAPrev.next;
+        }
+        listAPrev.next = tailPre.next;
+
+        int[] b = {5, 6, 1};
+        ListNode listBPrev = new ListNode();
+        ListNode listB = listBPrev;
+        for (int value : b) {
+            listBPrev.next = new ListNode(value);
+            listBPrev = listBPrev.next;
+        }
+        listBPrev.next = tailPre.next;
+        ListNode result = new Solution().getIntersectionNode2(listA.next, listB.next);
+        for (int value : tailArray) {
+            assertEquals(value, result.val);
+            result = result.next;
+        }
+    }
 }

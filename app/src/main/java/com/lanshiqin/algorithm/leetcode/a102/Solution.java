6a125990dc224116574d2ecbc7e4f547283adc16
lanshiqin <lanshiqin@outlook.com>
2022-02-22 23:26:58 +0800

    102. 二叉树的层序遍历

---
+++ app/src/main/java/com/lanshiqin/algorithm/leetcode/a102/Solution.java
@@ -0,0 +1,63 @@
+package com.lanshiqin.algorithm.leetcode.a102;
+
+import com.lanshiqin.algorithm.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ * 示例 2：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 示例 3：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int size = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            while (size > 0) {
+                TreeNode curr = queue.poll();
+                subList.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+                size--;
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+}

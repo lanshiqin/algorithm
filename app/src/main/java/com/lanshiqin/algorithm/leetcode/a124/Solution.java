5eb0fcdb8bf55b9e8d83978f33f2423e10e65d9a
lanshiqin <lanshiqin@outlook.com>
2022-03-08 21:24:40 +0800

    124. 二叉树中的最大路径和

---
+++ app/src/main/java/com/lanshiqin/algorithm/leetcode/a124/Solution.java
@@ -0,0 +1,50 @@
+package com.lanshiqin.algorithm.leetcode.a124;
+
+import com.lanshiqin.algorithm.leetcode.TreeNode;
+
+/**
+ * 124. 二叉树中的最大路径和
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ * <p>
+ * 路径和 是路径中各节点值的总和。
+ * <p>
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目范围是 [1, 3 * 104]
+ * -1000 <= Node.val <= 1000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        getMax(root);
+        return max;
+    }
+
+    public int getMax(TreeNode root){
+        if (root==null) return 0;
+        int leftMax = Math.max(0,getMax(root.left));
+        int rightMax = Math.max(0,getMax(root.right));
+        max = Math.max(max, root.val+leftMax + rightMax);
+        return Math.max(leftMax, rightMax) + root.val;
+    }
+}
---
+++ app/src/test/java/com/lanshiqin/algorithm/leetcode/a124/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.lanshiqin.algorithm.leetcode.a124;
+
+import com.lanshiqin.algorithm.leetcode.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void maxPathSum() {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        assertEquals(6,new Solution().maxPathSum(root));
+    }
+}

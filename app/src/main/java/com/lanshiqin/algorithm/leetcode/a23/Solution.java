673e182bf4c681d8c557ad5867a920a755019353
lanshiqin <lanshiqin@outlook.com>
2022-03-19 14:50:20 +0800

    23. 合并K个升序链表

---
+++ app/src/main/java/com/lanshiqin/algorithm/leetcode/a23/Solution.java
@@ -0,0 +1,72 @@
+package com.lanshiqin.algorithm.leetcode.a23;
+
+import com.lanshiqin.algorithm.leetcode.ListNode;
+
+/**
+ * 23. 合并K个升序链表
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：lists = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：lists = [[]]
+ * 输出：[]
+ * <p>
+ * 提示：
+ * <p>
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = (left + right) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeList(l1, l2);
+    }
+
+    public ListNode mergeList(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeList(l1, l2.next);
+            return l2;
+        }
+    }
+}

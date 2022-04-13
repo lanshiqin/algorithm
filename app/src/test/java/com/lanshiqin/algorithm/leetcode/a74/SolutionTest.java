21bbec953b789d783ef233ebfa8d70f0a3532316
lanshiqin <lanshiqin@outlook.com>
2022-04-13 20:56:45 +0800

    74. 搜索二维矩阵

--- app/src/test/java/com/lanshiqin/algorithm/leetcode/a74/SolutionTest.java
+++ app/src/test/java/com/lanshiqin/algorithm/leetcode/a74/SolutionTest.java
@@ -13,10 +13,10 @@ class SolutionTest {
 
     @Test
     void searchMatrix() {
-        assertTrue(new Solution().searchMatrix(new int[][]{{1}},1));
-        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3));
-        assertFalse(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50}},13));
-        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50}},10));
-        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1}}, 1));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3));
+        assertFalse(new Solution().searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 13));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 10));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3));
     }
 }

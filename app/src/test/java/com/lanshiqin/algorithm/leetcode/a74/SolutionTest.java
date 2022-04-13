f818b4b230140cd2bb08b8b69de5b29440075062
lanshiqin <lanshiqin@outlook.com>
2022-04-13 20:56:30 +0800

    74. 搜索二维矩阵

---
+++ app/src/test/java/com/lanshiqin/algorithm/leetcode/a74/SolutionTest.java
@@ -0,0 +1,22 @@
+package com.lanshiqin.algorithm.leetcode.a74;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void searchMatrix() {
+        assertTrue(new Solution().searchMatrix(new int[][]{{1}},1));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3));
+        assertFalse(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50}},13));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50}},10));
+        assertTrue(new Solution().searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3));
+    }
+}

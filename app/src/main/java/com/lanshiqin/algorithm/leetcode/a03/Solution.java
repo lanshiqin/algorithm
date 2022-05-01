c14e5b53c891bd2e4ce0946f773149a0231eccd6
lanshiqin <lanshiqin@outlook.com>
2022-05-01 10:09:25 +0800

    3. 无重复字符的最长子串,写法2双指针

--- app/src/main/java/com/lanshiqin/algorithm/leetcode/a03/Solution.java
+++ app/src/main/java/com/lanshiqin/algorithm/leetcode/a03/Solution.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 /**
  * 3. 无重复字符的最长子串
+ * <p>
  * 给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
  * <p>
  * <p>
@@ -13,16 +14,19 @@ import java.util.Map;
  * 输入: s = "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * <p>
  * 示例 2:
  * <p>
  * 输入: s = "bbbbb"
  * 输出: 1
  * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * <p>
  * 示例 3:
  * <p>
  * 输入: s = "pwwkew"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。
+ * <p>
  * 请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
  * <p>
  * <p>
@@ -33,7 +37,7 @@ import java.util.Map;
  * 通过次数1,502,150提交次数3,902,071
  * <p>
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 链接：<a href="https://leetcode-cn.com/problems/longest-substring-without-repeating-characters">https://leetcode-cn.com/problems/longest-substring-without-repeating-characters</a>
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
@@ -49,6 +53,26 @@ public class Solution {
             }
             map.put(curr, i);
             maxLen = Math.max(maxLen, i - left + 1);
+        }
+        return maxLen;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        if (s.isEmpty()) return 0;
+        int left = 0;
+        int right = 0;
+        int maxLen = 1;
+        int len = s.length() - 1;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0), 0);
+        while (right < len) {
+            right++;
+            Character curr = s.charAt(right);
+            if (map.containsKey(curr)) {
+                left = Math.max(left, map.get(curr) + 1);
+            }
+            map.put(curr, right);
+            maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
     }
--- app/src/test/java/com/lanshiqin/algorithm/leetcode/a03/SolutionTest.java
+++ app/src/test/java/com/lanshiqin/algorithm/leetcode/a03/SolutionTest.java
@@ -16,5 +16,15 @@ class SolutionTest {
         assertEquals(3,new Solution().lengthOfLongestSubstring("abcabcbb"));
         assertEquals(1,new Solution().lengthOfLongestSubstring("bbbbb"));
         assertEquals(3,new Solution().lengthOfLongestSubstring("pwwkew"));
+        assertEquals(1,new Solution().lengthOfLongestSubstring(" "));
+    }
+
+    @Test
+    void lengthOfLongestSubstring2() {
+        assertEquals(3,new Solution().lengthOfLongestSubstring2("abcabcbb"));
+        assertEquals(1,new Solution().lengthOfLongestSubstring2("bbbbb"));
+        assertEquals(3,new Solution().lengthOfLongestSubstring2("pwwkew"));
+        assertEquals(1,new Solution().lengthOfLongestSubstring2(" "));
+        assertEquals(2,new Solution().lengthOfLongestSubstring2("aab"));
     }
 }

523e3c983d56cc19f10dacfc74ee9007aab5d3e8
lanshiqin <lanshiqin@outlook.com>
2022-09-20 08:09:25 +0800

    冒泡优化，如果完全有序则提前退出

--- app/src/main/java/com/lanshiqin/algorithm/sort/BubbleSort.java
+++ app/src/main/java/com/lanshiqin/algorithm/sort/BubbleSort.java
@@ -12,18 +12,19 @@ public class BubbleSort {
     public void bubbleSort(int[] nums) {
         int n = nums.length;
         for (int i = n; i >= 1; i--) {
-            bubble(nums, i);
-        }
-    }
-
-    private void bubble(int[] nums, int n){
-        int tmp;
-        for (int i = 0; i < n - 1; i++) {
-            if(nums[i]>nums[i+1]){
-                tmp = nums[i+1];
-                nums[i+1] = nums[i];
-                nums[i] = tmp;
+            boolean flags = false;
+            for (int j = 0; j < n - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    flags = true;
+                }
+            }
+            if (!flags) {
+                break;
             }
         }
     }
+
 }

8ff0ae8903b46c7e388502f1a4b9605b0309cf88
lanshiqin <lanshiqin@outlook.com>
2022-09-27 08:16:09 +0800

    插入排序

--- app/src/main/java/com/lanshiqin/algorithm/sort/InsertSort.java
+++ app/src/main/java/com/lanshiqin/algorithm/sort/InsertSort.java
@@ -2,25 +2,31 @@ package com.lanshiqin.algorithm.sort;
 
 /**
  * 插入排序
+ * 分为已排序和待排序
  *
  * @author shiqin.lan
  */
 public class InsertSort {
 
-    public void insertSort(int[] nums){
-        for (int i = 1; i < nums.length; i++) {
-            insert(nums, i);
+    public void insertSort(int[] nums) {
+        int n = nums.length;
+        // 遍历未排序
+        for (int i = 1; i < n; i++) {
+            int value = nums[i];
+            // 从后往前遍历已排序
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                // 比较未排序和已排序的元素
+                if (value < nums[j]) {
+                    // 移动元素
+                    nums[j + 1] = nums[j];
+                } else {
+                    break;
+                }
+            }
+            // 插入元素
+            nums[j + 1] = value;
         }
     }
 
-    private void insert(int[] nums, int n){
-        int key = nums[n];
-        int i = n;
-        while(nums[i-1]>key){
-            nums[i] = nums[i-1];
-            i--;
-            if(i==0) break;
-        }
-        nums[i] = key;
-    }
 }
